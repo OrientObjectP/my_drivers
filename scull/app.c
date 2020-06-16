3bb5f84c57005a8eacb1feb4b60de44718ba5d5e
OrientObjectP <444042583@qq.com>
2020-06-17 03:14:05 +0800

    test app

--- scull/app.c
+++ scull/app.c
@@ -11,29 +11,25 @@
 #define BUFSIZE 100
 int main(void)
 {
-	char wbuf[BUFSIZE] = {0};
+	char wbuf1[BUFSIZE] = {0};
+	char wbuf2[BUFSIZE] = {0};  
     char r1buf[BUFSIZ] = {0};
     char r2buf[BUFSIZ] = {0};   
     int i = 0;
-	for (i = 0; i < sizeof(wbuf); i++) {
-		wbuf[i] = 'a' + i % 26;
+	for (i = 0; i < sizeof(wbuf1); i++) {
+		wbuf1[i] = 'a' + i % 26;
 	}
+	for (i = 0; i < sizeof(wbuf2); i++) {
+		wbuf2[i] = 'A' + i % 26;
+	}    
 
     int fd1 = open(PATH1,O_RDWR);
     int fd2 = open(PATH2,O_RDWR);
 
-    write(fd1,wbuf,50);
+    write(fd1,wbuf1,10);
+    write(fd2,wbuf2,50);
     read(fd1,r1buf,20);
-
-    read(fd2,r2buf,20);
-
-    printf("r1buf*****%s\n",r1buf);
-    printf("r2buf*****%s\n",r2buf);
-
-
-
-
-
+    printf("r2buf*****%s\n",r1buf);
 
 
     return 0;
